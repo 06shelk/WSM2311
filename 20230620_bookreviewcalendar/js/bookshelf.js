ecee88a15a88af8fab10d18f844bfcd8be476c2c
Seol <w2232@e-mirim.hs.kr>
2023-06-20 16:08:56 +0900

    bookshelf.js. 이미지 10개 가져오자

---
+++ 20230620_bookreviewcalendar/js/bookshelf.js
@@ -0,0 +1,19 @@
+console.log(books[3].img);
+
+//HTML->js
+//.book-item > img 를 가져오자
+const bookItems = document.getElementsByClassName("book-item"); //.book-item 덩어리 가져오자
+let i = 0;
+for (let bookItem of bookItems) { //.book-item 하나씩 꺼내자
+    const bookItemImg = bookItem.getElementsByTagName("img")[0]; //.book-item > 0 하나씩 꺼내자
+    bookItemImg.src = books[i].img; //거기에 data.js에서 데이터 가져와 넣기
+    i++;
+}
+
+
+// const bookItems = document.getElementsByClassName("book-item");
+// console.log(bookItems);
+// bookItem = bookItems[0];
+// const bookItemImg = bookItem.getElementsByTagName("img")[0];
+// console.log(bookItemImg);
+// bookItemImg.src = books[3].img;
