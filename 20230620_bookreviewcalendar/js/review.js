51d43d8d3e163231202f669223ddd6e655c636b2
Seol <w2232@e-mirim.hs.kr>
2023-08-17 10:00:28 +0900

    Add index.html link in 책장 menu

--- 20230620_bookreviewcalendar/js/review.js
+++ 20230620_bookreviewcalendar/js/review.js
@@ -1,54 +1,78 @@
-// URL에 있는 book parameter 값(String) -> String을 int로 변환 -> -1 -> books의 인덱스로 변환(int)
+// 내가 짠 코드
+// const urlParams = new URL(location.href).searchParams;
+// const url = urlParams.get('book');
+// const number = (parseInt(url) - 1);
 
-//URL에 있는 book parameter 값 (string)
-let url_href = window.location.href; // 브라우저 창에 있는 주소
-const bookId = new URL(url_href).searchParams.get("book"); 
-console.log(`bookId: ${bookId}, type: ${typeof bookId}`);
-// -> string 을 number로 변환: bookIdNumber
-//let bookIdNumber= Number(bookId); // string -> number
-let bookIdNumber = parseInt(bookId); // string -> number
-//let bookIdNumber = bookId * 1; // string -> number
-//let bookIdNumber = bookId + 0; // string -> number
+// const id = books[number].id; 
+// console.log(`bookId: ${id}`);
 
-console.log(`bookIdNumber: ${bookIdNumber}, type: ${typeof bookIdNumber}`);
+// const title = books[number].title; 
+// console.log(`bookTitle: ${title}`);
 
-//books에서 하나씩 꺼내어 bookIdNumber랑 꺼낸 book의 id랑 같으면 데이터 빼자: bookData
-//bookIdNumber: URL에서 가져온 id 값
-//bookIdNumber: URL에서 가져온 id 값 === books 하나씩 꺼낸 book의 id 값
+// 수업
+
+
+// const bookId = books[0].id;
+// console.log(`bookId : ${bookId}`);
+// const bookTitle = books[0].title;
+// console.log(`bookTitle : ${bookTitle}`);
+// const bookAuthor = books[0].author;
+// console.log(`bookAuthor : ${bookAuthor}`);
+
+
+// URL이 있는 book parameter 값(string) 
+let url_href = window.location.href;    //브라우저 창에 있는 주
+const bookId = new URL(url_href).searchParams.get("book");
+console.log(`bookId: ${bookId}, type : ${typeof bookId}`);
+//bookId가 null이면, index.html로 가자
+if(bookId === null){
+    window.location.href = 'index.html';
+    
+    // var link = 'index.html';
+    // location.href=link;
+    // location.replace(link);
+    //indow.open(link);
+
+    //window.open("index.html", target="_top"); // 새로운 창 열려서 이동하는 문제 해결
+}
+
+
+//-> String 을 number로 변환:bookIdNumber 
+let bookIdNumber = parseInt(bookId);    //string->number
+// let bookIdNumber = Number(bookId);         //string->number
+// let bookIdNumber = bookId * 1;          //string->number
+
+console.log(`bookIdNumber: ${bookIdNumber}, type : ${typeof bookIdNumber}`);  //3, number
+
+//-> books에서 하나씩 꺼내어서 bookIdNumber랑 꺼낸 book의 id랑 같으면 데이터 뺴자 :bookData
+//bookIdNumber: URL에서 가져온 id값 === books 하나씩 꺼낸 book의 id 값
 let bookData;
 for(let book of books) {
-    if(bookIdNumber === book.id) { // == 값을 비교, === 타입까지 비교
-        bookData = book;    //id가 같으면, 그 놈을 꺼내서 bookData에 넣자
+    if(bookIdNumber === book.id) {
+        bookData = book;    //id가 같으면, 걔를 꺼내서 bookData에 넣자
         break;
     }
 }
 console.log(bookData);
 
+
 // -> title, author, publisher, bookImage 알아내자
 let title = bookData.title;
-//let title = bookData["title"];
-console.log(title);
 
 let author = bookData.author;
 let publisher = bookData.publisher;
-let bookImage = bookData.bookImage;
+let bookImage = bookData.img;
 console.log(title, author, publisher, bookImage);
 
 // -> HTML에 넣자
+// HTML요소 => js 변수
+const titleDiv = document.getElementsByClassName("title")[1];   //  
+titleDiv.innerHTML = title;
+// titleDiv.innerHTML = `<u>${title}</u>`;
+const authorDiv = document.getElementsByClassName("author")[1];
+authorDiv.innerHTML = author;
+const publisherDiv = document.getElementsByClassName("publisher")[1];
+publisherDiv.innerHTML = publisher;
 
-
-
-// 현재 페이지의 URL에서 쿼리 파라미터를 가져옵니다.
-let url = new URL(location.href).searchParams;
-
-// "book" 파라미터의 값을 가져옵니다.
-let book = url.get("book");
-
-// 콘솔에 book 값을 출력합니다.
-console.log(book);
- 
-// searchParams는 URL 객체의 프로퍼티로, 주소 뒤에 오는 쿼리 문자열(query string)을 다루기 위한 API입니다. 
-// 쿼리 문자열은 URL 뒤에 ? 문자로 시작하며, key=value 형식으로 파라미터를 나타내는 것을 말합니다.
-
-const bookTitle = books[0].title;
-console.log(`bookTitle: ${bookTitle}`)
+const bookImageDiv = document.getElementsByClassName("book-image")[0];
+bookImageDiv.innerHTML = `<img src="${bookImage}"/>`;
