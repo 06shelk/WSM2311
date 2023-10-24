a2f552370678a8d2635322ac7d607c2c1e161277
Seol <w2232@e-mirim.hs.kr>
2023-10-24 15:36:31 +0900

    bootstrap 1. utilities, Background, Spacing, Colors, Text, Borders

--- 20230601_nav/js/nav.js
+++ 20230601_nav/js/nav.js
@@ -1,45 +1,4 @@
-//.nav-toggle 클릭하면
-//.nav-toggle i 햄버거 메뉴 <-> X
-//.nav-list에 .show menu toggle
 
-//HTml -> js 
-// 1. ID
-//const navToggleDiv = document.getElementById("nav-toggle");
-// 2. class
-//const navToggleDiv = document.getElementsByclassName("nav-toggle")[0];
-// 요소가 여러개 s 하나라도 s 임.
-// 3. Tag
-//const navToggleDiv = document.getElementsByTagName("div")[0];
-// 4. css selector
-// const navToggleDiv = document.querySelector("#nav-toggle"); 
-// const navToggleDiv = document.querySelectorAll(".nav-toggle")[0];
-// const navToggleDiv = document.querySelectorAll("div")[0];
-// 5. Xpath
-
-
-//nav_toggle.onclick = () => alert("안녕");
-//nav_toggle.onclick =  alert("안녕");
-
-/* function sayHello(){
-    alert("안녕");
-}
-
-nav_toggle.onclick = function(){
-    alert("안녕");
-};
- */
-//" 안녕이라는 창을 띄우는 함수를 실행해라"
-// sayHello();
-// nav_toggle.onclick = sayHello;
-
-// nav_toggle.onclick = () =>{
-// alert("안녕");
-// };
-//nav_toggle.onclick = sayHello;
-
-//nav_toggle.onclick = () => alert("안녕");
-
-//
 const navToggleI = nav_toggle.getElementsByTagName("i")[0];
 const navListUl = document.getElementsByClassName("nav-list")[0];
 
